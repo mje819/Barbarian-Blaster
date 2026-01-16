24661410d8422ed2a4bfc46126c663fb2097e574
mje819 <2260211@flourbluffschools.org>
2026-01-16 12:37:45 -0600

    create enemy and path 3d

---
+++ Enemy/enemy.gd
@@ -0,0 +1,13 @@
+extends PathFollow3D
+
+## The speed in m/s the enemy will move
+@export var speed := 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	progress += speed * delta
