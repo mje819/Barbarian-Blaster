791890bc25d5e99c698e72fdd198385e575c408e
mje819 <2260211@flourbluffschools.org>
2026-01-20 11:57:20 -0600

    create base

---
+++ Base/base.gd
@@ -0,0 +1,6 @@
+extends Node3D
+class_name Base
+
+func take_damage() -> void:
+	print("damage taken by base")
+	
--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,8 +1,9 @@
 extends PathFollow3D
 
-## The speed in m/s the enemy will move
+## The speed in m/s the enemy will move (reset back to 6.5)
 @export var speed := 10.0
 
+@onready var base : Base = get_tree().get_first_node_in_group("base")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,3 +12,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress += speed * delta
+	if progress_ratio == 1:
+		base.take_damage()
+		queue_free()
