bc00b5822ccaf8838cdfb812fa433b1d6f6d4493
mje819 <2260211@flourbluffschools.org>
2026-01-21 12:19:37 -0600

    working health label

--- Base/base.gd
+++ Base/base.gd
@@ -7,13 +7,18 @@ var health : int :
 	set(new_health):
 		health = new_health
 		label_3d.text = str(new_health) + "/" + str(max_health)
+		var red : Color = Color.RED
+		var white : Color = Color.WHITE
+		
+		label_3d.modulate = red.lerp(white, float(health)/max_health)
+		
 		if health == 0:
 			await get_tree().create_timer(1).timeout
 			get_tree().reload_current_scene()
 
 func _ready() -> void:
 	health = max_health
-	label_3d.text = str(max_health) + "/" + str(max_health)
+	#label_3d.modulate = Color.RED
 
 func take_damage() -> void:
 	health -= 1
