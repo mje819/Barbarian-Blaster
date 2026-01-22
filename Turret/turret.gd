4cfc03eb9f83c1a2e425e13892f864847074b1b5
mje819 <2260211@flourbluffschools.org>
2026-01-22 12:40:40 -0600

    rotate turret wip

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,7 +1,12 @@
 extends Node3D
 
 var projectile = preload("res://Turret/projectile.tscn")
+var enemy_path : Path3D
 
+# turn to enemies
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
 
 func _on_timer_timeout() -> void:
 	var projectile_instance = projectile.instantiate()
--- turret_manager.gd
+++ turret_manager.gd
@@ -2,8 +2,10 @@ extends Node3D
 class_name TurretManager
 
 @export var turret : PackedScene
+@export var enemy_path : Path3D
 
 func build_turret(pos : Vector3) -> void:
 	var turret_instance = turret.instantiate()
 	add_child(turret_instance)
 	turret_instance.global_position = pos
+	turret_instance.enemy_path = enemy_path
